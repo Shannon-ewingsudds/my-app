9e7344a86d34cc2ec3afd27930183e6114d3a6ad
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-03-23 17:14:14 -0400

    update to git

---
+++ src/TodoList.js
@@ -0,0 +1,36 @@
+function TodoList() {
+    return (
+
+        <
+        div >
+
+        <
+        h1 > "Shannon's Todo List": < /h1>  
+
+        <
+        ul >
+        <
+        li > < input type = "checkbox" / > Finish projects at work < /li>
+
+        <
+        li > < input type = "checkbox" / > Complete homework assignments < /li>
+
+        <
+        li > < input type = "checkbox" / > Fold laundry < /li> 
+
+        <
+        li > < input type = "checkbox" / > Do the dishes < /li> 
+
+        <
+        li > < input type = "checkbox" / > Complete volunteer work < /li> 
+
+        <
+        /
+        ul >
+
+        <
+        /div >
+    );
+}
+
+export default TodoList;
